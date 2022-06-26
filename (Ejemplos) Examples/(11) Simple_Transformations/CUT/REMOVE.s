8d48cb25ca85138f9901ffc5b774a91630fde6b1
Sebastian Mayorquin <sebastianmayorquin@gmail.com>
2022-06-26 12:34:15 +0100

    Explicacion Transformaciones Dinamicas + Utiles: REMOVE.s, CUT.s, SHIFT.s

---
+++ (Ejemplos) Examples/(11) Simple_Transformations/CUT/REMOVE.s
@@ -0,0 +1,33 @@
+# Subrutina que elimina una imagen dinámica
+# Argumentos:
+# a0: Direccion de imagen
+
+		.globl REMOVE
+		.eqv REMOVE_COLOR 0 # (Negro)
+REMOVE:
+
+		.text
+		#-- Comienzo subruina
+		addi sp, sp, -4
+		sw ra, 0(sp)
+		
+		mv t0,a0		 # Copiamos direccion de ID
+		
+		# Borramos datos inestables
+		li a0,0
+		li a1,0
+		li a2, 0		
+		li a3, 0
+		
+		# Colocamos un rectangulo negro encima de la imagen
+		lh a0, 8(t0)	 # Coordenada X
+		lh a1, 10(t0)    # Coordenada Y
+		lh a2, 12(t0)	# Ancho
+		lh a3, 14(t0)	# Alto
+		li a4,REMOVE_COLOR		# (Negro)
+		jal RECTANGLE
+		
+		lw ra, 0(sp) #-- Recurperar el registro ra de la pila
+		addi sp, sp, 4
+		ret
+		
---
+++ (Ejemplos) Examples/(11) Simple_Transformations/SHIFT (Dinamic Move)/MOVE.s
@@ -0,0 +1,190 @@
+# Programa que realiza una traslación estática de una imagen dada hacia una direccion determinada. (Solo traslaciones ortogonales) Utilizable en display 512x512
+# Argumentos:
+# a0: Puntero a Imagen
+# a1: Ancho
+# a2: Alto
+# a3: Coordenada x Inicial
+# a4: Coordenada y Inicial
+# a5: Orientación
+# - 0: Derecha
+# - 1: Abajo
+# - 2: Izquierda
+# - 3: Arriba
+# a6: Desplazamiento
+		.globl MOVE
+		.eqv SIZE 512
+		.eqv DISPLAY 0x10040000
+		.eqv TRAIL_COLOR 0
+
+MOVE:
+		.text
+		# Inicio Subrutina
+		addi sp, sp, -64
+		sw ra, 60(sp)
+				
+		# Inicializacion de valores para comprobacion de desplazamiento
+		li t0,0
+		li t1,1
+		li t2,2
+		li t3,3
+		
+		li t4,0 # Contador
+		
+		# Comprobacion de valor de desplazamiento
+		beq a5,t0,derecha
+		beq a5,t1, abajo
+		beq a5,t2, izquierda
+		beq a5,t3, arriba
+		
+derecha:
+	beq t4, a6, fin
+	jal Guardado
+	
+	# Obtencion de coordenadas
+	mv a0,a3			# Coordenada x
+	mv a1,a4			# Coordenada y
+	li a2, SIZE		# Display
+	li a3, DISPLAY		# Puntero a display
+	jal COORD
+	sw a0, 24(sp)		# Guarda el resultado
+	
+	# Creacion de Linea Negra
+	jal Carga
+	lw a0, 24(sp)		# Puntero -> a0
+	addi a0, a0, -4	# Correción de puntero
+	
+	mv a1,a2	 		# Distancia (Alto) -> a1
+	li a2, TRAIL_COLOR	# Color (Negro) -> a2
+	li a3, SIZE		# Display -> a3
+	jal V_LINE		# Haz una linea vertical negra a la izquierda
+	
+	# Impresion de nueva imagen
+	jal Carga
+	jal IMAGE		# Imprime Imagen
+	jal Carga
+	
+	# Cambio de pixel
+	addi a3,a3,1		# Desplaza un pixel a la derecha
+	addi t4,t4,1		# Aumenta en 1 el contador
+	
+	b derecha
+
+izquierda:
+	beq t4, a6, fin
+	jal Guardado
+	
+	# Obtencion de coordenadas
+	mv a0,a3			# Coordenada x
+	add a0,a0, a1		# Movemos la coordenada a la derecha de la imagen
+	mv a1,a4			# Coordenada y
+	li a2, SIZE		# Display
+	li a3, DISPLAY		# Puntero a display
+	jal COORD
+	sw a0, 24(sp)		# Guarda el resultado
+	
+	# Creacion de Linea Negra
+	jal Carga
+	lw a0, 24(sp)		# Puntero -> a0
+	mv a1,a2	 		# Distancia (Alto) -> a1
+	li a2, TRAIL_COLOR	# Color (Negro) -> a2
+	li a3, SIZE		# Display -> a3
+	jal V_LINE		# Haz una linea vertical negra a la izquierda
+	
+	# Impresion de nueva imagen
+	jal Carga
+	jal IMAGE		# Imprime Imagen
+	jal Carga
+	
+	# Cambio de pixel
+	addi a3,a3,-1		# Desplaza un pixel a la izquierda
+	addi t4,t4,1		# Aumenta en 1 el contador
+	
+	b izquierda
+
+arriba:
+	beq t4, a6, fin
+	jal Guardado
+	
+	# Obtencion de coordenadas
+	mv a0,a3			# Coordenada x
+	mv a1,a4			# Coordenada y
+	add a1,a1, a2		# Movemos la coordenada a la esquina inferior de la imagen
+	li a2, SIZE		# Display
+	li a3, DISPLAY		# Puntero a display
+	jal COORD
+	sw a0, 24(sp)		# Guarda el resultado
+	
+	# Creacion de Linea Negra
+	jal Carga
+	lw a0, 24(sp)		# Puntero -> a0
+	li a2, TRAIL_COLOR	# Color (Negro) -> a2
+	jal H_LINE		# Haz una linea horizontal desde abajo
+	
+	# Impresion de nueva imagen
+	jal Carga
+	jal IMAGE		# Imprime Imagen
+	jal Carga
+	
+	# Cambio de pixel
+	addi a4,a4,-1		# Desplaza un pixel hacia arriba
+	addi t4,t4,1		# Aumenta en 1 el contador
+	
+	b arriba
+
+abajo:
+	beq t4, a6, fin
+	jal Guardado
+	
+	# Obtencion de coordenadas
+	mv a0,a3			# Coordenada x
+	mv a1,a4			# Coordenada y
+	addi a1,a1,-1		# Correcion de linea
+	li a2, SIZE		# Display
+	li a3, DISPLAY		# Puntero a display
+	jal COORD
+	sw a0, 24(sp)		# Guarda el resultado
+	
+	# Creacion de Linea Negra
+	jal Carga
+	lw a0, 24(sp)		# Puntero -> a0
+	li a2, TRAIL_COLOR	# Color (Negro) -> a2
+	jal H_LINE		# Haz una linea horizontal desde arriba
+	
+	# Impresion de nueva imagen
+	jal Carga
+	jal IMAGE		# Imprime Imagen
+	jal Carga
+	
+	# Cambio de pixel
+	addi a4,a4,1		# Desplaza un pixel hacia abajo
+	addi t4,t4,1		# Aumenta en 1 el contador
+	
+	b abajo
+
+Guardado:
+		# Guardado de datos
+		sw a0, 56(sp)
+		sw a1, 52(sp)
+		sw a2, 48(sp)
+		sw a3, 44(sp)
+		sw a4, 40(sp)
+		sw a6, 32(sp)
+		sw t4, 28(sp)
+		ret
+Carga:
+
+		# Carga de datos
+		lw a0, 56(sp)
+		lw a1, 52(sp)
+		lw a2, 48(sp)
+		lw a3, 44(sp)
+		lw a4, 40(sp)
+		lw a6, 32(sp)
+		lw t4, 28(sp)
+		ret
+		
+fin:
+		#-- Fin de subrutina
+		lw ra, 60(sp) #-- Recurperar el registro ra de la pila
+		addi sp, sp, 64
+		ret
